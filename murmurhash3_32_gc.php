efe73ab821c7d97a55d9f4f77bed1148afb4dfec
Stefano Azzolini <lastguest@gmail.com>
2015-02-13 12:33:44 +0100

    Speed optimizations.

--- murmurhash3_32_gc.php
+++ murmurhash3_32_gc.php
@@ -28,13 +28,13 @@ function murmurhash3_32_gc($key,$seed=0){
               ((ord($key[++$i]) & 0xff) << 16) |
               ((ord($key[++$i]) & 0xff) << 24);
         ++$i;
-        $k1 = (((($k1 & 0xffff) * $c1) + ((((int)((float)$k1 /0x10000) * $c1) & 0xffff) << 16))) & 0xffffffff;
-        $k1 = ($k1 << 15) | (int)((float)$k1 /pow(2,(int)17));
-        $k1 = (((($k1 & 0xffff) * $c2) + ((((int)((float)$k1 /0x10000) * $c2) & 0xffff) << 16))) & 0xffffffff;
+        $k1 = (((($k1 & 0xffff) * $c1) + (((($k1 >> 16) * $c1) & 0xffff) << 16))) & 0xffffffff;
+        $k1 = $k1 << 15 | $k1 >> 17;
+        $k1 = (((($k1 & 0xffff) * $c2) + (((($k1 >> 16) * $c2) & 0xffff) << 16))) & 0xffffffff;
         $h1 ^= $k1;
-        $h1 = ($h1 << 13) | (int)((float)$h1 /pow(2,(int)19));
-        $h1b = (((($h1 & 0xffff) * 5) + ((((int)((float)$h1 /0x10000) * 5) & 0xffff) << 16))) & 0xffffffff;
-        $h1 = ((($h1b & 0xffff) + 0x6b64) + ((((int)((float)$h1b /0x10000) + 0xe654) & 0xffff) << 16));
+        $h1 = $h1 << 13 | $h1 >> 19;
+        $h1b = (((($h1 & 0xffff) * 5) + (((($h1 >> 16) * 5) & 0xffff) << 16))) & 0xffffffff;
+        $h1 = ((($h1b & 0xffff) + 0x6b64) + (((($h1b >> 16) + 0xe654) & 0xffff) << 16));
     }
    
     $k1 = 0;
@@ -42,16 +42,16 @@ function murmurhash3_32_gc($key,$seed=0){
         case 3: $k1 ^= (ord($key[$i + 2]) & 0xff) << 16;
         case 2: $k1 ^= (ord($key[$i + 1]) & 0xff) << 8;
         case 1: $k1 ^= (ord($key[$i]) & 0xff);
-        $k1 = ((($k1 & 0xffff) * $c1) + ((((int)((float)$k1 /0x10000) * $c1) & 0xffff) << 16)) & 0xffffffff;
-        $k1 = ($k1 << 15) | (int)((float)$k1 /pow(2,(int)17));
-        $k1 = ((($k1 & 0xffff) * $c2) + ((((int)((float)$k1 /0x10000) * $c2) & 0xffff) << 16)) & 0xffffffff;
+        $k1 = ((($k1 & 0xffff) * $c1) + (((($k1 >> 16) * $c1) & 0xffff) << 16)) & 0xffffffff;
+        $k1 = $k1 << 15 | $k1 >> 17;
+        $k1 = ((($k1 & 0xffff) * $c2) + (((($k1 >> 16) * $c2) & 0xffff) << 16)) & 0xffffffff;
         $h1 ^= $k1;
     }
     $h1 ^= $klen;
-    $h1 ^= (int)((float)$h1 /0x10000);
-    $h1 = ((($h1 & 0xffff) * 0x85ebca6b) + ((((int)((float)$h1 /0x10000) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
-    $h1 ^= (int)((float)$h1 /pow(2,(int)13));
-    $h1 = (((($h1 & 0xffff) * 0xc2b2ae35) + ((((int)((float)$h1 /0x10000) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
-    $h1 ^= (int)((float)$h1 /0x10000);
-    return base_convert((int)$h1,10,32);
+    $h1 ^= $h1 >> 16;
+    $h1 = ((($h1 & 0xffff) * 0x85ebca6b) + (((($h1 >> 16) * 0x85ebca6b) & 0xffff) << 16)) & 0xffffffff;
+    $h1 ^= $h1 >> 13;
+    $h1 = (((($h1 & 0xffff) * 0xc2b2ae35) + (((($h1 >> 16) * 0xc2b2ae35) & 0xffff) << 16))) & 0xffffffff;
+    $h1 ^= $h1 >> 16;
+    return base_convert($h1,10,32);
 }
